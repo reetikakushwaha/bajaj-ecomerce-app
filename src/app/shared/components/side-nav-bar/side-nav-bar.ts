aa0847e9eda4f7fa357822e0fdaf2fda5ad71b29
reetikakushwaha <jireetika@gmail.com>
2025-10-24 21:36:06 +0530

    final-commit

--- src/app/shared/components/side-nav-bar/side-nav-bar.ts
+++ src/app/shared/components/side-nav-bar/side-nav-bar.ts
@@ -8,5 +8,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./side-nav-bar.css'] 
 })
 export class SideNavBar {
- 
+  ngAfterViewInit() {
+  const toggler = document.querySelector('.navbar-toggler');
+  const navbar = document.querySelector('.custom-navbar');
+  const collapse = document.getElementById('navbarContent');
+
+  if (toggler && navbar && collapse) {
+    collapse.addEventListener('shown.bs.collapse', () => {
+      navbar.classList.add('show-height');
+    });
+
+    collapse.addEventListener('hidden.bs.collapse', () => {
+      navbar.classList.remove('show-height');
+    });
+  }
+}
+
 }
